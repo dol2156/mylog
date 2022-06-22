505b1053eaebb6db4c77252622ae4a58ba629980
dol2156 <dol2156@gmail.com>
2022-06-22 15:09:25 +0900

    Update PageMain.vue

--- src/views/PageMain.vue
+++ src/views/PageMain.vue
@@ -1,23 +1,40 @@
 <template>
+  <div class="create_webtoon_item_box">
+    <input type="text" placeholder="웹툰 제목 입력"
+           v-model="this.create_webtoon_title"
+           @keyup.enter="onCreateWebToonItem"
+    />
+    <button class="create_btn" type="button"
+            @click="onCreateWebToonItem"
+    >추가
+    </button>
+  </div>
   <ul class="webtoon_list">
     <li v-for="(item, idx) in webtoon_list" :key="item.id" :data-idx="idx">
-      <button class="삭제" type="button">삭제</button>
+      <button class="삭제" type="button"
+              @click="onRemoveWebToonItem(item)"
+      >삭제
+      </button>
       <div class="제목">
         <div class="text_0">{{ item.id }}</div>
         <div class="text_1">{{ item.제목 }}</div>
       </div>
       <input
         class="회차" type="number" placeholder="placeholder"
-        :value="item.회차"/>
+        v-model="item.회차" onclick="this.select();"/>
       <button class="저장" type="button"
               @click="onSaveClick(item)"
-      >저장</button>
+      >저장
+      </button>
     </li>
   </ul>
 </template>
 <script>
 import {getFirestore} from "firebase/firestore";
-import {collection, getDocs} from "firebase/firestore";
+import {collection, doc, getDocs, addDoc, updateDoc, deleteDoc} from "firebase/firestore";
+
+let app, db;
+let collectionName = "webtoon";
 
 export default {
   name : "PageMain",
@@ -25,48 +42,153 @@ export default {
     return {
       message : "Hello Vue",
       webtoon_list : [],
+      create_webtoon_title : "",
     };
   },
   created() {
     // 인스턴스가 생성된 후 동기적으로 호출
     console.log("PageLogin created");
-    
     // Initialize Firebase
-    const app = this.$_Firebase;
-    const db = getFirestore(app);
-    getDocs(collection(db, "webtoon"))
-      .then((response) => {
-        // success
-        // console.log(response);
-        let arr = [];
-        response.forEach((doc) => {
-          let data = doc.data();
-          data.id = doc.id;
-          arr.push(data);
-          //console.log(`${doc.id} => ${doc.data()}`);
-        });
-        console.log(arr);
-        this.webtoon_list = arr;
-      })
-      .catch((error) => {
-        // error
-        console.log(error);
-      })
-      .then(() => {
-        // complete
-      })
+    app = this.$_Firebase;
+    db = getFirestore(app);
+    this.readWebToonItem();
   },
   methods : {
+    makeWebToonObj(제목, 회차) {
+      const obj = {
+        제목,
+        회차,
+      }
+      return obj;
+    },
+    
+    readWebToonItem() {
+      getDocs(collection(db, collectionName))
+        .then((response) => {
+          // success
+          // console.log(response);
+          let arr = [];
+          response.forEach((doc) => {
+            let data = doc.data();
+            data.id = doc.id;
+            arr.push(data);
+            //console.log(`${doc.id} => ${doc.data()}`);
+          });
+          console.log(arr);
+          this.webtoon_list = arr;
+        })
+        .catch((error) => {
+          // error
+          console.log(error);
+        })
+        .then(() => {
+          // complete
+        })
+    },
+    
+    createWebToonItem(제목) {
+      addDoc(collection(db, collectionName),
+        this.makeWebToonObj(제목, 0))
+        .then((response) => {
+          // success
+          console.log(response);
+          this.readWebToonItem();
+          this.create_webtoon_title = '';
+        })
+        .catch((error) => {
+          // error
+          console.log(error);
+        })
+        .then(() => {
+          // complete
+        });
+    },
+    
+    updateWebToonItem(item) {
+      
+      const id = item.id;
+      const 회차 = item.회차;
+      const ref = doc(db, collectionName, id);
+      updateDoc(ref, {
+        회차
+      })
+        .then(() => {
+          this.readWebToonItem();
+        })
+        .catch((error) => {
+          // error
+          console.log(error);
+        })
+        .then(() => {
+          // complete
+        });
+    },
+    
+    onCreateWebToonItem() {
+      const title = this.create_webtoon_title.trim();
+      this.createWebToonItem(title);
+    },
+    
+    onRemoveWebToonItem(item) {
+      if(!window.confirm('삭제?')) return;
+      
+      const id = item.id;
+      // eslint-disable-next-line no-unreachable
+      const ref = doc(db, collectionName, id);
+      deleteDoc(ref)
+        .then(() => {
+          // success
+          this.readWebToonItem();
+        })
+        .catch((error) => {
+          // error
+          console.log(error);
+        })
+        .then(() => {
+          // complete
+        });
+    },
+    
     onSaveClick(item) {
-      console.log(item);
-      item.회차 = 10;
+      
+      this.updateWebToonItem(item);
+      
+      /*
+      const ref = doc(db, collectionName, id);
+      setDoc(ref, this.makeWebToonObj("title", "number"))
+        .then(() => {
+        })
+        .catch((error) => {
+          // error
+          console.log(error);
+        })
+        .then(() => {
+          // complete
+        });
+       */
     },
   },
 }
 </script>
 <style lang="scss" scoped>
 $FORM_EL_SIZE:50px;
+.create_webtoon_item_box{
+  display:flex; flex-wrap:nowrap; align-items:center;
+  
+  > input{
+    display:block;
+    font-size:16px;
+    width:100%; height:$FORM_EL_SIZE; padding:0 10px;
+  }
+  
+  > .create_btn{
+    min-width:$FORM_EL_SIZE; width:$FORM_EL_SIZE; height:$FORM_EL_SIZE;
+    background-color:green; color:white;
+  }
+}
 .webtoon_list{
+  margin-top:20px;
+  
   > li{
     border:1px solid #dddddd;
     display:flex; align-items:center; flex-wrap:nowrap; gap:5px;
