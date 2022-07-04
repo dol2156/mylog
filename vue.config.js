cd664dfd9260d5f56b86a0273a83585d801d7c75
강석민 <dol2156@gmail.com>
2022-07-04 11:01:37 +0900

    dd

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  outputDir: 'docs',
-  publicPath: '/mylog',
-  css: {
-    sourceMap: true,
-  },
-})
